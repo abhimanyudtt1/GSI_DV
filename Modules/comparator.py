ea698dd529928dfef49b91026821a9bf476703cd
abhimanyu.dutta <abhimanyu.dutta@guavus.com>
2018-03-07 14:22:48 +0530

    # Comparator changes # 2

--- Modules/comparator.py
+++ Modules/comparator.py
@@ -14,4 +14,6 @@ def comparator(df1,df2):
     df_gpby = bigDf.groupby(list(bigDf.columns))
     idx = [x[0] for x in df_gpby.groups.values() if len(x) == 1]
     bigDf = bigDf.reindex(idx)
-    print bigDf
+    print df1.head()
+    print df2.head()
+    print bigDf.head()
